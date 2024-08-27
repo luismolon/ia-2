860030538c51530ea96bec811955ef5af36612e9
Luis Molon <107118364+luismolon@users.noreply.github.com>
2024-08-27 14:50:54 -0300

    Add files via upload

---
+++ IA/js/aleatorio.js
@@ -0,0 +1,8 @@
+const nomes = ["Fernanda", "Luis", "Maria Eduarda", "guilherme", "Amanda", "Gustavo", "Gabriel"];
+
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
